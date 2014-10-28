de1b47653f829926128cfc861f9ee4af096578bb
Axel <axel.rl.404@gmail.com>
2014-10-28 15:01:17 +0700

    Update SpreadOfFire.java

--- spreadoffire/SpreadOfFire.java
+++ spreadoffire/SpreadOfFire.java
@@ -15,7 +15,8 @@ public class SpreadOfFire {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        TUI();
+        JFrame myGUI=new Controller();
+        //TUI();
     }
     
     public static void TUI(){
